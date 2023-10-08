9fa4f9380695b91bef8fc5054181bf5959f75293
Dawid Frankiewicz <dawid.frankiewicz2001@gmail.com>
2023-10-08 20:27:43 +0200

    REmoved unused styles, added config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            charset: 'utf-8',
+            title: 'Display Cards',
+            htmlAttrs: {
+                lang: 'en',
+            },
+        }
+    },
     devtools: { enabled: true },
     css: ['~/assets/css/main.css'],
     postcss: {
